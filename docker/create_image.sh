1c5d96c5ed113493e7cbbe351dc937c0b79dedbe
a-sanchezgu-2019 <a.sanchezgu.2019@alumnos.urjc.es>
2025-07-01 10:24:31 +0200

    Docker Fix

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ "$1" = "backend"]; then
+if [ "$1" = "backend" ]; then
     docker build -f openvidu-call-polls-backend.Dockerfile -t openvidu-call-polls ..
 else
     docker build -f Dockerfile -t openvidu-call-polls ..
